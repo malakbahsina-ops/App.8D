b2a209d059f66d6ff2a44498720407cd01a130dd
malakbahsina-ops <malak.bahsina@uit.ac.ma>
2025-12-31 01:01:58 +0100

    Fix duplicate paths in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -5,3 +5,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+path('login/', ...)
+path('refresh/', ...)
